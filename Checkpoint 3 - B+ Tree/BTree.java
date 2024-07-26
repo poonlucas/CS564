bf76f32e54e68d31d005d49ba968ed3b0fca0763
shreyasnat2804 <shreyas.2003.natarajan@gmail.com>
2024-07-26 08:34:46 +0530

    added code for insertion, need to fix

--- Checkpoint 3 - B+ Tree/BTree.java
+++ Checkpoint 3 - B+ Tree/BTree.java
@@ -56,12 +56,126 @@ class BTree {
         return result; // Return recordId or -1 if not found
     }
 
+    private void insertNonFull(BTreeNode node, Student student) {
+        int i = node.n - 1;
+
+        if (node.leaf) {
+            while (i >= 0 && student.studentId < node.keys[i]) {
+                node.keys[i + 1] = node.keys[i];
+                node.values[i + 1] = node.values[i];
+                i--;
+            }
+
+            node.keys[i + 1] = student.studentId;
+            node.values[i + 1] = student.recordId;
+            node.n++;
+
+            if (node.n == 2 * t) {
+                split(node);
+            }
+        } else {
+            while (i >= 0 && student.studentId < node.keys[i]) {
+                i--;
+            }
+            i++;
+
+            if (node.children[i].n == 2 * t - 1) {
+                split(node.children[i]);
+
+                if (student.studentId > node.keys[i]) {
+                    i++;
+                }
+            }
+            insertNonFull(node.children[i], student);
+        }
+    }
+
+    private void split(BTreeNode node) {
+        int mid = t - 1;
+        BTreeNode newNode = new BTreeNode(t, node.leaf);
+
+        for (int j = 0; j < t - 1; j++) {
+            newNode.keys[j] = node.keys[j + t];
+            if (node.leaf) {
+                newNode.values[j] = node.values[j + t];
+            }
+        }
+
+        if (!node.leaf) {
+            for (int j = 0; j < t; j++) {
+                newNode.children[j] = node.children[j + t];
+            }
+        }
+
+        newNode.n = t - 1;
+        node.n = t - 1;
+
+        if (node.leaf) {
+            newNode.next = node.next;
+            node.next = newNode;
+        }
+
+        if (node == root) {
+            BTreeNode newRoot = new BTreeNode(t, false);
+            newRoot.keys[0] = node.keys[mid];
+            newRoot.children[0] = node;
+            newRoot.children[1] = newNode;
+            newRoot.n = 1;
+            root = newRoot;
+        } else {
+            BTreeNode parent = findParent(root, node);
+            int i = parent.n - 1;
+            while (i >= 0 && node.keys[mid] < parent.keys[i]) {
+                parent.keys[i + 1] = parent.keys[i];
+                parent.children[i + 2] = parent.children[i + 1];
+                i--;
+            }
+            parent.keys[i + 1] = node.keys[mid];
+            parent.children[i + 2] = newNode;
+            parent.n++;
+        }
+    }
+
+    private BTreeNode findParent(BTreeNode current, BTreeNode child) {
+        if (current == null || current.leaf) {
+            return null;
+        }
+
+        for (int i = 0; i <= current.n; i++) {
+            if (current.children[i] == child) {
+                return current;
+            }
+            BTreeNode parent = findParent(current.children[i], child);
+            if (parent != null) {
+                return parent;
+            }
+        }
+
+        return null;
+    }
+
+
     BTree insert(Student student) {
         /**
          * TODO:
          * Implement this function to insert in the B+Tree.
          * Also, insert in student.csv after inserting in B+Tree.
          */
+
+         if (root == null) {
+            root = new BTreeNode(t, true);
+            root.keys[0] = student.studentId;
+            root.values[0] = student.recordId;
+            root.n = 1;
+        } else {
+            if (root.n == 2 * t - 1) {
+                BTreeNode newRoot = new BTreeNode(t, false);
+                newRoot.children[0] = root;
+                split(root);
+                root = newRoot;
+            }
+            insertNonFull(root, student);
+        }
         return this;
     }
 
