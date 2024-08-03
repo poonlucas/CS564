16561c8514e30836860c802650dc673909277555
shreyasnat2804 <shreyas.2003.natarajan@gmail.com>
2024-08-03 14:39:57 +0530

    changed setup

--- setup/setup.py
+++ setup/setup.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 username = 'root'
-password = 'root'
+password = 'password'
 host = 'localhost'
 database = 'moviesdb'
 FILES = ["user_taggedmovies-timestamps.dat",
@@ -16,7 +16,6 @@ FILES = ["user_taggedmovies-timestamps.dat",
          "user_ratedmovies-timestamps.dat",
          "user_taggedmovies.dat"]
 dfs = dict()
-
 
 def download_zip():
     print("Downloading dataset...")
@@ -31,7 +30,6 @@ def download_zip():
 
     os.remove("temp.zip")
 
-
 def read_dats():
     print("Reading dataset...")
     for file in FILES:
@@ -39,56 +37,44 @@ def read_dats():
         dfs[file] = df
         os.remove(file)
 
-
 def insert():
-
-    engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}/{database}')
+    engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}/{database}')
 
     print("Populating directors...")
     directors_dict = {k: v for v, k in enumerate(dfs[FILES[1]].directorName.unique())}
-    directors = pd.DataFrame(directors_dict.items())[[1, 0]]. \
-        rename(columns={1: 'director_id', 0: 'director_name'})
+    directors = pd.DataFrame(directors_dict.items())[[1, 0]].rename(columns={1: 'director_id', 0: 'director_name'})
     directors.to_sql('directors', engine, if_exists='append', index=False)
 
     print("Populating tags...")
-    tags = dfs[FILES[4]]. \
-        rename(columns={'id': 'tag_id', 'value': 'tag_name'})
+    tags = dfs[FILES[4]].rename(columns={'id': 'tag_id', 'value': 'tag_name'})
     tags.to_sql('tags', engine, if_exists='append', index=False)
 
     print("Populating movies...")
-    movies = dfs[FILES[3]][['id', 'title', 'year', 'imdbPictureURL']]. \
-        rename(columns={'id': 'movie_id', 'imdbPictureURL': 'imdb_picture_url'})
+    movies = dfs[FILES[3]][['id', 'title', 'year', 'imdbPictureURL']].rename(columns={'id': 'movie_id', 'imdbPictureURL': 'imdb_picture_url'})
     movies.to_sql('movies', engine, if_exists='append', index=False)
 
     print("Populating directs...")
     dfs[FILES[1]]['director_id'] = dfs[FILES[1]]['directorName'].map(directors_dict)
-    directs = dfs[FILES[1]][['director_id', 'movieID']]. \
-        rename(columns={'movieID': 'movie_id'})
+    directs = dfs[FILES[1]][['director_id', 'movieID']].rename(columns={'movieID': 'movie_id'})
     directs.to_sql('directs', engine, if_exists='append', index=False)
 
     print("Populating user_tags...")
-    user_tags = dfs[FILES[0]]. \
-        drop(columns=['timestamp']). \
-        rename(columns={'userID': 'user_id', 'movieID': 'movie_id', 'tagID': 'tag_id'})
+    user_tags = dfs[FILES[0]].drop(columns=['timestamp']).rename(columns={'userID': 'user_id', 'movieID': 'movie_id', 'tagID': 'tag_id'})
     user_tags.to_sql('user_tags', engine, if_exists='append', index=False)
 
     print("Populating ratings...")
-    ratings = dfs[FILES[5]][['userID', 'movieID', 'rating']]. \
-        rename(columns={'userID': 'user_id', 'movieID': 'movie_id'})
+    ratings = dfs[FILES[5]][['userID', 'movieID', 'rating']].rename(columns={'userID': 'user_id', 'movieID': 'movie_id'})
     ratings.to_sql('ratings', engine, if_exists='append', index=False)
 
     print("Populating genre...")
     genre_dict = {k: v for v, k in enumerate(dfs[FILES[2]].genre.unique())}
-    genre = pd.DataFrame(genre_dict.items())[[1, 0]]. \
-        rename(columns={1: 'genre_id', 0: 'genre_name'})
+    genre = pd.DataFrame(genre_dict.items())[[1, 0]].rename(columns={1: 'genre_id', 0: 'genre_name'})
     genre.to_sql('genre', engine, if_exists='append', index=False)
 
     print("Populating has_genre...")
     dfs[FILES[2]]['genre_id'] = dfs[FILES[2]]['genre'].map(genre_dict)
-    has_genre = dfs[FILES[2]][['genre_id', 'movieID']]. \
-        rename(columns={'movieID': 'movie_id'})
+    has_genre = dfs[FILES[2]][['genre_id', 'movieID']].rename(columns={'movieID': 'movie_id'})
     has_genre.to_sql('has_genre', engine, if_exists='append', index=False)
-
 
 if __name__ == '__main__':
     download_zip()
