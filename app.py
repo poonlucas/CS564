ef7887bdeee83c1d5861d9d5ca39d8804a209541
Lucas Poon <lucaslfpoon@gmail.com>
2024-08-04 23:33:43 -0700

    more work on recommendmovie

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine, text
 from collections import OrderedDict
 
 
+
 def create_app():
     nav = Nav()
     nav.register_element(
@@ -37,26 +38,71 @@ def create_app():
         genre = request.args.get('genre')
         tag = request.args.get('tag')
 
-        sql = f'''SELECT top_movies.title, top_movies.average_rating, g.genre_name, t.tag_name
-        FROM (
-            SELECT hg.movie_id, avg_ratings.title, avg_ratings.average_rating
+        sql = '''SELECT top_movies.title, top_movies.average_rating, g.genre_name, t.tag_name
+        FROM ('''
+
+        if genre == "" and tag == "":
+            sql += '''
+                SELECT m.movie_id, m.title, AVG(r.rating) AS average_rating
+                FROM movies m
+                JOIN ratings r ON m.movie_id = r.movie_id
+                GROUP BY m.movie_id, m.title
+                ORDER BY average_rating DESC
+                LIMIT 10'''
+        elif genre != "" and tag == "":
+            sql += f'''
+                SELECT hg.movie_id, avg_ratings.title, avg_ratings.average_rating
+                FROM genre g
+                JOIN has_genre hg ON g.genre_id = hg.genre_id
+                JOIN (
+                    SELECT m.movie_id, m.title, AVG(r.rating) AS average_rating
+                    FROM movies m
+                    JOIN ratings r ON m.movie_id = r.movie_id
+                    GROUP BY m.movie_id, m.title
+                ) as avg_ratings ON hg.movie_id = avg_ratings.movie_id
+                WHERE g.genre_name = '{genre}'
+                ORDER BY average_rating DESC
+                LIMIT 10'''
+        elif genre == "" and tag != "":
+            sql += f'''
+            SELECT ut.movie_id, avg_ratings.title, avg_ratings.average_rating, t.tag_name
+            FROM tags t
+            JOIN user_tags ut ON t.tag_id = ut.tag_id
+            JOIN (
+                SELECT m.movie_id, m.title, AVG(r.rating) AS average_rating
+                FROM movies m
+                JOIN ratings r ON m.movie_id = r.movie_id
+                GROUP BY m.movie_id, m.title
+            ) as avg_ratings ON ut.movie_id = avg_ratings.movie_id
+            WHERE t.tag_name = 'dogs'
+            ORDER BY average_rating DESC
+            LIMIT 10'''
+        elif genre != "" and tag != "":
+            sql += f'''
+            SELECT hg.movie_id, avg_ratings.title, avg_ratings.average_rating, g.genre_name
             FROM genre g
             JOIN has_genre hg ON g.genre_id = hg.genre_id
+            JOIN user_tags ut ON hg.movie_id = ut.movie_id
+            JOIN tags t ON ut.tag_id = t.tag_id
             JOIN (
                 SELECT m.movie_id, m.title, AVG(r.rating) AS average_rating
                 FROM movies m
                 JOIN ratings r ON m.movie_id = r.movie_id
                 GROUP BY m.movie_id, m.title
             ) as avg_ratings ON hg.movie_id = avg_ratings.movie_id
-            WHERE g.genre_name = '{genre}'
+            WHERE g.genre_name = '{genre}' AND t.tag_name = '{tag}'
             ORDER BY average_rating DESC
-            LIMIT 10
+            LIMIT 10'''
+
+        sql += '''
         ) AS top_movies
         LEFT JOIN has_genre hg ON top_movies.movie_id = hg.movie_id
         LEFT JOIN genre g ON hg.genre_id = g.genre_id
         LEFT JOIN user_tags ut ON top_movies.movie_id = ut.movie_id
         LEFT JOIN tags t ON ut.tag_id = t.tag_id
         ORDER BY top_movies.average_rating DESC;'''
+
+        print(sql)
 
         movie_dict = OrderedDict()
         with engine.connect() as conn:
@@ -73,9 +119,10 @@ def create_app():
 
         movies = []
         for title, (average_rating, genre, tags) in movie_dict.items():
-            movies.append((title, average_rating, ', '.join(str(genre) for genre in genre), ', '.join(str(tag) for tag in tags)))
+            movies.append(
+                (title, average_rating, ', '.join(str(genre) for genre in genre), ', '.join(str(tag) for tag in tags)))
 
-        return render_template('recommendmovie.html', movies = movies)
+        return render_template('recommendmovie.html', movies=movies)
 
     return app
 
