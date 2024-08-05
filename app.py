954da91699502d7f692077ebafa700e78f54ef90
Lucas Poon <lucaslfpoon@gmail.com>
2024-08-04 23:03:58 -0700

    recommendmovie

--- app.py
+++ app.py
@@ -35,10 +35,11 @@ def create_app():
     @app.route('/get-movie-recommendation', methods=['GET'])
     def get_movie_recommendation():
         genre = request.args.get('genre')
-        sql = f'''
-        SELECT top_movies.title, top_movies.average_rating, top_movies.genre_name, t.tag_name
+        tag = request.args.get('tag')
+
+        sql = f'''SELECT top_movies.title, top_movies.average_rating, g.genre_name, t.tag_name
         FROM (
-            SELECT hg.movie_id, avg_ratings.title, avg_ratings.average_rating, g.genre_name
+            SELECT hg.movie_id, avg_ratings.title, avg_ratings.average_rating
             FROM genre g
             JOIN has_genre hg ON g.genre_id = hg.genre_id
             JOIN (
@@ -51,23 +52,28 @@ def create_app():
             ORDER BY average_rating DESC
             LIMIT 10
         ) AS top_movies
+        LEFT JOIN has_genre hg ON top_movies.movie_id = hg.movie_id
+        LEFT JOIN genre g ON hg.genre_id = g.genre_id
         LEFT JOIN user_tags ut ON top_movies.movie_id = ut.movie_id
         LEFT JOIN tags t ON ut.tag_id = t.tag_id
-        ORDER BY top_movies.average_rating DESC;
-        '''
+        ORDER BY top_movies.average_rating DESC;'''
+
         movie_dict = OrderedDict()
         with engine.connect() as conn:
             result = conn.execute(text(sql))
             for row in result:
                 if row[0] not in movie_dict:
-                    movie_dict[row[0]] = (row[1], row[2], [row[3]])
+                    movie_dict[row[0]] = (row[1], [row[2]], [row[3]])
                 else:
-                    _, _, tags = movie_dict[row[0]]
-                    tags.append(row[3])
+                    _, genre, tags = movie_dict[row[0]]
+                    if row[2] not in genre:
+                        genre.append(row[2])
+                    if row[3] not in tags:
+                        tags.append(row[3])
 
         movies = []
         for title, (average_rating, genre, tags) in movie_dict.items():
-            movies.append((title, average_rating, genre, ', '.join(str(tag) for tag in tags)))
+            movies.append((title, average_rating, ', '.join(str(genre) for genre in genre), ', '.join(str(tag) for tag in tags)))
 
         return render_template('recommendmovie.html', movies = movies)
 
