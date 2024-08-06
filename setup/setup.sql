f3dbaa68c4d68120431ac3b3f0083d9871a0002a
Lucas Poon <lucaslfpoon@gmail.com>
2024-08-05 20:26:40 -0700

    updated sql with stored procedure

--- setup/setup.sql
+++ setup/setup.sql
@@ -58,3 +58,39 @@ CREATE TABLE IF NOT EXISTS user_tags(
     FOREIGN KEY (tag_id) REFERENCES tags(tag_id),
     FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
 );
+
+DELIMITER //
+CREATE PROCEDURE insert_movie_with_director(IN director_name VARCHAR(255), IN title VARCHAR(255), IN year INT, IN imdb_picture_url VARCHAR(255))
+BEGIN
+	DECLARE n_director_id BIGINT;
+    DECLARE n_movie_id BIGINT;
+
+    SELECT d.director_id INTO n_director_id
+    FROM directors d
+    WHERE d.director_name = director_name;
+
+    IF n_director_id IS NULL THEN
+		SET n_director_id = (SELECT MAX(director_id) FROM directors);
+        INSERT INTO directors VALUES (n_director_id, director_name);
+	END IF;
+
+    SET n_movie_id = (SELECT MAX(m.movie_id) + 1 FROM movies m);
+    INSERT INTO movies VALUES (n_movie_id, title, year, imdb_picture_url);
+    INSERT INTO directs VALUES (n_director_id, n_movie_id);
+    SELECT n_movie_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE delete_movie(IN movie_id BIGINT)
+BEGIN
+	DELETE FROM has_genre hg WHERE hg.movie_id = movie_id;
+    DELETE FROM directs dir WHERE dir.movie_id = movie_id;
+    DELETE FROM user_tags ut WHERE ut.movie_id = movie_id;
+    DELETE FROM movies m WHERE m.movie_id = movie_id;
+    SELECT * FROM movies m WHERE m.movie_id = movie_id;
+END //
+DELIMITER ;
+
+
+
